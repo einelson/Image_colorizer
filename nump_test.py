a5c656ffa5dfe086a159716efdcbb2d339841033
Ethan Nelson <36721383+einelson8i@users.noreply.github.com>
2020-07-21 20:10:28 -0600

    train model updated
    Fixed issue with gathering and fitting the data. There is an issue with creating the NN still

--- nump_test.py
+++ nump_test.py
@@ -6,15 +6,21 @@ from tensorflow.keras.preprocessing.image import img_to_array
 
 # print(var.shape)
 # print(var)
+storage=list()
 
 img1=np.array(img_to_array(load_img("./TestImages/0AEYvu.jpg")), dtype=float)
 # print(img1.shape)
+storage.append(img1)
 
 img2=np.array(img_to_array(load_img("./TestImages/0AEYvu.jpg")), dtype=float)
+storage.append(img2)
 
 img3=np.array(img_to_array(load_img("./TestImages/0AEYvu.jpg")), dtype=float)
+storage.append(img3)
 
-imgs = np.stack((img1,img2,img3), axis=3)
+
+print('expected')
+imgs = np.stack((storage), axis=3)
 print(imgs.shape)
 # print all info from 4th dimension
-print(imgs[:,:,:,1])
+# print(imgs[:,:,:,1])
--- train_model.py
+++ train_model.py
@@ -10,27 +10,31 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import (array_to_img, img_to_array,load_img)
 from tqdm import tqdm
 
-
+# Converts RGB valuse to LAB
 def get_lab(img):
     l = rgb2lab(img/255)[:,:,0]
     return l
 
+# Returns the AB values from the LAB
 def get_color(img):
     x = rgb2lab(img/255)[:,:,1:] # this is the A and B values; a-magenta-green; b-yellow-blue
     x/=128
     return x
 
+# Gets the images and returns them in a 4d np.array format
 def get_images(path, color="lab"):
-    images = np.array()
-    for filename in os.listdir(path):
+    images = list()
+    print('Loading Images')
+    for filename in tqdm(os.listdir(path)):
         if filename[0] != '.':
             if color == "lab":
                 img = get_lab(np.array(img_to_array(load_img(path + filename)), dtype=float))
-                images.append(img.reshape(1,img.shape[0],img.shape[1],1))
+                images.append(img.reshape(img.shape[0],img.shape[1],1))
             else:
                 img = get_color(np.array(img_to_array(load_img(path + filename)), dtype=float))
-                images.append(img.reshape(1,img.shape[0],img.shape[1],2))
-    return images
+                images.append(img.reshape(img.shape[0],img.shape[1],2))
+    image_array=np.stack(images, axis=3)            
+    return image_array
 
 '''
 Convert all training images from the RGB color space to the Lab color space.
@@ -38,18 +42,21 @@ Use the L channel as the input to the network and train the network to predict t
 Combine the input L channel with the predicted ab channels.
 Convert the Lab image back to RGB.
 '''
+# Get the LAB values
 x = get_images("./OurTrainingImages/") #l value only
 print(len(x))
 y = get_images("./OurTrainingImages/", color="yes") #a and b values
 
-print(x.shape())
+# Check the array shape (256,256,1,x(number of images))
+print(x.shape)
+print(y.shape)
 
 # Recreate the exact same model, including its weights and the optimizer
 # model = tf.keras.models.load_model('./img_predictions/model.h5')
 
-# create model
+# create model needs to be remade for a 4d array...
 model = Sequential()
-model.add(InputLayer(input_shape=(None, None, 1))) # input shape is only needed for first layer? input_shape=(256, 256, 3)
+model.add(InputLayer(input_shape=(None, None, 1, None))) # input shape is only needed for first layer? input_shape=(256, 256, 3)
 # 3x3 kernel used and 8 filters?
 model.add(Conv2D(8, (3, 3), activation='relu', padding='same', strides=2))
 model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
@@ -64,21 +71,14 @@ model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
 model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(2, (3,3), activation='tanh', padding='same'))
-# get working after we get NN working better
-'''
-# supposed to soften image
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
-'''
+
 # get summary of layers and compile
 model.summary()
 model.compile(optimizer='adam',loss='mse') # loss='sparse_categorical_crossentropy', optomizer='rmsprop'
 
 
 # there is an issue fitting the data
-for e in tqdm(range(10000)):
-    for i,j in enumerate(x):
-        model.fit(x=x[i],y=y[i], batch_size=50,verbose=0, epochs=1)
+model.fit(x=x,y=y, batch_size=50,verbose=0, epochs=1000)
 
 # evaluate model
 # model.evaluate(x, y, batch_size=1)
